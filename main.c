42907896ea15b3d17d50596a1064c08357d4049a
José Areia <jose.apareia@gmail.com>
2021-10-19 19:06:16 +0100

    edit(err_values): set new errors values as macros and other verifications

--- main.c
+++ main.c
@@ -23,9 +23,191 @@
 #include "memory.h"
 #include "type.h"
 
-void handle_signal(int signal, siginfo_t * siginfo, void *context);
+#define ERR_CMD_LINE    1
+#define ERR_SIGNAL      2
+#define ERR_EXEC        3
+#define ERR_FORK        4
+#define ERR_OPEN_FILE   5
+#define ERR_CLOSE_FILE  6
 
-void handle_signal(int signal, siginfo_t * siginfo, void *context) {
+void open_output_file();
+void handle_signal(int signal, siginfo_t *siginfo, void *context);
+
+int main(int argc, char *argv[]) {
+    struct gengetopt_args_info args;
+    struct sigaction act;
+
+    /* Declare and set all the variables used for statistic purposes at zero */
+    int num_files = 0, num_errors = 0, num_ok = 0, num_mismatch = 0;
+    int status;
+
+    if(cmdline_parser(argc, argv, &args) != 0) ERROR(ERR_CMD_LINE, "cannot execute cmdline_parser\n");
+
+    /* Signals */
+    act.sa_sigaction = handle_signal;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags |= SA_SIGINFO;
+
+    /* If the mode isn't -b/--batch the signal SIGUSR1 is ignored by the application */
+    if (args.batch_arg != NULL) {
+        if (sigaction(SIGUSR1, &act, NULL) < 0) {
+            ERROR(ERR_SIGNAL, "Failed to execute sigaction (SIGUSR1)");
+        }
+    } else {
+        signal(SIGUSR1, SIG_IGN);
+    }
+
+    if (sigaction(SIGQUIT, &act, NULL) < 0) {
+        ERROR(ERR_SIGNAL, "Failed to execute sigaction (SIGQUIT)");
+    }
+
+    if (sigaction(SIGINT, &act, NULL) < 0) {
+        ERROR(ERR_SIGNAL, "Failed to execute sigaction (SIGINT)");
+    }
+
+    printf("The application is ready to receive the signals SIGQUIT and SIGUSR1.\nThe signal SIGUSR1 will only work for the batch '-b/--batch' mode.\nIn order to send signals use the following PID: %d\n", getpid());
+
+    /* -f/--file option */
+    if ((int) args.file_given > 0) {
+        for (int i = 0; i < (int) args.file_given; i++) {
+            /* Check if the file passed throught the command line exists or not. */
+            if (access(args.file_arg[i], F_OK) == -1) {
+                fprintf(stderr, "ERROR: cannot open file '%s' -- %s\n", args.file_arg[i], strerror(errno));
+            } else {
+                pid_t pid = fork();
+                if (pid == 0) {
+                    /* Opens the file which is going to receive the STDOUT_FILENO */
+                    open_output_file();
+                    execlp("file", "file", "-b", "--mime-type", args.file_arg[i], NULL);
+                    ERROR(ERR_EXEC, "Failed to execute execlp");
+                    exit(EXIT_FAILURE);
+                } else if (pid > 0) {
+                    waitpid(pid, &status, 0);
+                    if (WIFEXITED(status)) {
+                        if (WEXITSTATUS(status) == ERR_EXEC) {
+                            exit(EXIT_FAILURE);
+                        } else {
+                            FILE *fd = fopen("output.txt", "r");
+                            if (fd == NULL) ERROR(ERR_OPEN_FILE, "cannot open the file 'output.txt'");
+                            char *mime = MALLOC(sizeof(char) + 1);
+                            fscanf(fd, "%s", mime);
+                            check_mime(mime, args.file_arg[i], &num_ok, &num_mismatch);
+                            free(mime);
+                            fclose(fd);
+                        }
+                    }
+                } else {
+                    ERROR(ERR_FORK, "Failed to execute fork");
+                }
+            }
+        }
+        pause();
+        exit(EXIT_SUCCESS);
+    }
+
+    /* -b/--batch option */
+    if (args.batch_arg != NULL) {
+        if (access(args.batch_arg, F_OK) == -1) {
+            fprintf(stderr, "ERROR: cannot open file '%s' -- %s\n", args.batch_arg, strerror(errno));
+        } else {
+            FILE *file_input = fopen(args.batch_arg, "r");
+            if (file_input == NULL) ERROR(ERR_OPEN_FILE, "Failed to open the file '%s'", args.batch_arg);
+            printf("Please send a SIGUSR1 in order to proceed with the application in batch mode.\n");
+            pause();
+            printf("[INFO] analysing files listed in '%s'\n", args.batch_arg);
+            char *files = MALLOC(sizeof(char) + 1);
+            while (fscanf(file_input, "%s", files) != EOF) {
+                printf("Processing number: %d/%s\n", num_files + 1, files);
+                if (access(files, F_OK) == -1) {
+                    fprintf(stderr, "[ERROR] cannot open file '%s' -- %s\n", files, strerror(errno));
+                    num_errors++;
+                } else {
+                    pid_t pid = fork();
+                    if (pid == 0) {
+                        open_output_file();
+                        execlp("file", "file", "-b", "--mime-type", files, NULL);
+                        ERROR(ERR_EXEC, "Failed to execute execlp");
+                        exit(EXIT_FAILURE);
+                    } else if (pid > 0) {
+                        waitpid(pid, &status, 0);
+                        if (WIFEXITED(status)) {
+                            if (WEXITSTATUS(status) == ERR_EXEC) {
+                                exit(EXIT_FAILURE);
+                            } else {
+                                FILE *fd = fopen("output.txt", "r");
+                                if (fd == NULL) ERROR(ERR_OPEN_FILE, "cannot open the file 'output.txt'");
+                                char *mime = MALLOC(sizeof(char) + 1);
+                                while (fscanf(fd, "%s", mime) != EOF) {
+                                    check_mime(mime, files, &num_ok, &num_mismatch);
+                                }
+                                free(mime);
+                                fclose(fd);
+                            }
+                        }
+                    } else {
+                        ERROR(ERR_FORK, "Failed to execute fork");
+                    }
+                }
+                num_files++;
+            }
+            free(files);
+            printf("[SUMMARY] files analysed:%d; files OK:%d; files MISMATCH:%d, errors:%d\n", num_files, num_ok, num_mismatch, num_errors);
+            exit(EXIT_SUCCESS);
+        }
+    }
+
+    /* -d/--dir option */
+    if (args.dir_arg != NULL){
+        DIR *folder;
+        struct dirent *dir;
+        folder = opendir(args.dir_arg);
+
+        if (folder == NULL) {
+            fprintf(stderr, "ERROR: cannot open dir '%s' -- %s\n", args.dir_arg, strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        while((dir=readdir(folder)) != NULL) {
+            if(strcmp (dir->d_name, ".") == 0 || strcmp (dir->d_name, "..") == 0) {
+                continue;
+            }
+
+            pid_t pid = fork();
+            if (pid == 0) {
+                strcat(args.dir_arg, dir->d_name);
+                open_output_file();
+                execlp("file", "file", "-b", "--mime-type", args.dir_arg, NULL); //exec to tell what type of file is
+                ERROR(ERR_EXEC, "Failed to execute execlp");
+                exit(EXIT_FAILURE);
+            } else if (pid > 0) {
+                waitpid(pid, &status, 0);
+                if (WIFEXITED(status)) {
+                    if (WEXITSTATUS(status) == ERR_EXEC) {
+                        exit(EXIT_FAILURE);
+                    } else {
+                        FILE *fd = fopen("output.txt", "r");
+                        char *mimes = MALLOC(sizeof(char) + 1);
+                        fscanf(fd, "%s", mimes);
+                        check_mime(mimes, dir->d_name, &num_ok, &num_mismatch);
+                        free(mimes);
+                        fclose(fd);
+                    }
+                }
+            } else {
+                ERROR(ERR_FORK, "Failed to execute fork");
+            }
+            num_files++;
+        }
+        closedir(folder);
+        printf("[SUMMARY] files analysed:%d; files OK:%d; files MISMATCH:%d, errors:%d\n", num_files, num_ok, num_mismatch, num_errors);
+        pause();
+        exit(EXIT_SUCCESS);
+    }
+	cmdline_parser_free(&args);
+    return 0;
+}
+
+void handle_signal(int signal, siginfo_t *siginfo, void *context) {
     (void) context;
     int aux;
     aux = errno;
@@ -36,6 +218,7 @@ void handle_signal(int signal, siginfo_t * siginfo, void *context) {
     }
 
     if (signal == SIGINT) {
+        printf("A SIGINT signal was captured (sent by PID: %ld). Terminating the application...\n", (long) siginfo->si_pid);
         exit(EXIT_SUCCESS);
     }
 
@@ -59,169 +242,18 @@ void handle_signal(int signal, siginfo_t * siginfo, void *context) {
     errno = aux;
 }
 
-int main(int argc, char *argv[]) {
-    struct gengetopt_args_info args;
-    struct sigaction act;
-
-    /* Declare and set all the variables used for statistic purposes at zero */
-    int num_files = 0, num_errors = 0, num_ok = 0, num_mismatch = 0;
-
-    if(cmdline_parser(argc, argv, &args) != 0) {
-        ERROR(1, "Can't resolve cmdline_parser\n");
+/* Opens the file which contains all the mimes (has all the verifications needed)*/
+void open_output_file() {
+    int fd = open("output.txt", O_TRUNC | O_WRONLY | O_CREAT, S_IRWXU);
+    if (fd == -1) {
+        ERROR(ERR_OPEN_FILE, "Failed to open the file 'output.txt'");
     }
 
-    act.sa_sigaction = handle_signal;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags |= SA_SIGINFO;
+    /* Write the std_out to the file 'output.txt' */
+    dup2(fd, STDOUT_FILENO);
 
-    if (args.batch_arg != NULL) {
-        if (sigaction(SIGUSR1, &act, NULL) < 0) {
-            ERROR(1, "ERROR in sigaction - SIGUSR1");
-        }
-    } else {
-        signal(SIGUSR1, SIG_IGN);
+    int close = close(fd);
+    if (close == -1) {
+        ERROR(ERR_CLOSE_FILE, "Failed to close the file 'output.txt'");
     }
-
-    if (sigaction(SIGQUIT, &act, NULL) < 0) {
-        ERROR(1, "ERROR in sigaction - SIGQUIT");
-    }
-
-    if (sigaction(SIGINT, &act, NULL) < 0) {
-        ERROR(1, "ERROR in sigaction - SIGINT");
-    }
-
-    printf("The program is ready to receive the signals SIGQUIT and SIGUSR1 (just for batch '-b/--batch' mode).\nProgram PID: %d\n", getpid());
-
-    /* -f/--file option */
-    if ((int) args.file_given > 0) {
-        for (int i = 0; i < (int) args.file_given; ++i) {
-            /* Check if the file passed throught the command line exists or not. */
-            if (access(args.file_arg[i], F_OK) == -1) {
-                fprintf(stderr, "ERROR: cannot open file ''%s' -- %s\n", args.file_arg[i], strerror(errno));
-            } else {
-                pid_t pid = fork();
-                if (pid == 0) {
-                    int fd = open("output.txt", O_TRUNC | O_WRONLY | O_CREAT, S_IRWXU);
-                    dup2(fd, STDOUT_FILENO);
-                    close(fd);
-                    execlp("file", "file", "-E", "-b", "--mime-type", args.file_arg[i], NULL);
-                    perror("execl");
-                    exit(EXIT_FAILURE);
-                } else if (pid > 0) {
-                    wait(NULL);
-
-                    FILE *fp = fopen("output.txt", "r");
-                    if (fp == NULL) {
-                       exit(EXIT_FAILURE);
-                    }
-
-                    char *mime = MALLOC(sizeof(char) + 1);
-                    fscanf(fp, "%s", mime);
-                    check_mime(mime, args.file_arg[i], &num_ok, &num_mismatch);
-                    free(mime);
-                    fclose(fp);
-                } else {
-                    ERROR(1, "Can't execute fork()");
-                }
-            }
-        }
-        pause();
-        exit(EXIT_SUCCESS);
-    }
-
-    /* -b/--batch option */
-    if (args.batch_arg != NULL) {
-        if (access(args.batch_arg, F_OK) == 0) {
-            FILE * file_input = fopen(args.batch_arg, "r");
-            if (file_input == NULL) {
-               exit(EXIT_FAILURE);
-            }
-            pause();
-            printf("[INFO] analysing files listed in '%s'\n", args.batch_arg);
-            char *files = MALLOC(sizeof(char) + 1);
-            while (fscanf(file_input, "%s", files) != EOF) {
-                printf("Processing number: %d/%s\n", num_files+1, files);
-                if (access(files, F_OK) == -1) {
-                    fprintf(stderr, "[ERROR] cannot open file '%s' -- %s\n", files, strerror(errno));
-                    num_errors++;
-                } else {
-                    pid_t pid = fork();
-                    if (pid == 0) {
-                        int file_input = open("output.txt", O_TRUNC | O_WRONLY | O_CREAT, S_IRWXU);
-                        dup2(file_input, STDOUT_FILENO);
-                        close(file_input);
-                        execlp("file", "file", "-E", "-b", "--mime-type", files, NULL);
-                        perror("execl");
-                        exit(EXIT_FAILURE);
-                    } else if (pid > 0) {
-                        wait(NULL);
-                        FILE *file_output = fopen("output.txt", "r");
-                        if (file_output == NULL) {
-                           exit(EXIT_FAILURE);
-                        }
-                        char *mime = MALLOC(sizeof(char)+1);
-                        while (fscanf(file_output, "%s", mime) != EOF) {
-                            check_mime(mime, files, &num_ok, &num_mismatch);
-                        }
-                        free(mime);
-                        fclose(file_output);
-                    } else {
-                        ERROR(1, "Can't execute fork");
-                    }
-                }
-                num_files++;
-            }
-            printf("[SUMMARY] files analysed:%d; files OK:%d; files MISMATCH:%d, errors:%d\n", num_files, num_ok, num_mismatch, num_errors);
-            exit(EXIT_SUCCESS);
-       }
-       else {
-           fprintf(stderr, "ERROR: cannot open file '%s' -- %s\n", args.batch_arg, strerror(errno));
-       }
-    }
-
-    /* -d/--dir option */
-    if (args.dir_arg != NULL){
-        DIR *folder;
-        struct dirent *dir;
-        folder = opendir(args.dir_arg); //opens directory passed in argument
-
-        if (folder == NULL) {
-            fprintf(stderr, "ERROR: cannot open dir '%s' -- %s\n", args.dir_arg, strerror(errno));
-            exit(EXIT_FAILURE);
-        }
-
-        while((dir=readdir(folder)) != NULL) { //read all files of the directory
-            if(strcmp (dir->d_name, ".") == 0 || strcmp (dir->d_name, "..") == 0) {
-                continue;
-            }
-            pid_t pid = fork();
-            if (pid == 0) {
-                strcat(args.dir_arg, dir->d_name);
-                int fdin = open("output.txt", O_TRUNC | O_WRONLY | O_CREAT, S_IRWXU); //create a file to be written stdin and result from execlp
-                dup2(fdin, STDOUT_FILENO);
-                close(fdin);
-                execlp("file", "file", "-E", "-b", "--mime-type", args.dir_arg, NULL); //exec to tell what type of file is
-                perror("execlp");
-                exit(EXIT_FAILURE);
-            } else if (pid > 0) {
-                wait(NULL);
-                FILE *fdout = fopen("output.txt", "r"); //open output file
-                char *mimes = MALLOC(sizeof(char)+1);
-                fscanf(fdout, "%s", mimes); //write to mimes
-                check_mime(mimes, dir->d_name, &num_ok, &num_mismatch); //function that checks and compares every file extension(in external file named type.c)
-                fclose(fdout);
-                free(mimes);
-            } else {
-                ERROR(1, "Can't execute fork()");
-            }
-            num_files++;
-        }
-        closedir(folder);
-        printf("[SUMMARY] files analysed:%d; files OK:%d; files MISMATCH:%d, errors:%d\n", num_files, num_ok, num_mismatch, num_errors);
-        pause();
-        exit(EXIT_SUCCESS);
-    }
-
-	cmdline_parser_free(&args);
-    return 0;
 }
--- type.c
+++ type.c
@@ -19,61 +19,61 @@
 
 #define NUM_OF_MIMES 7
 
-int match_mime(const char *buf_extension, char **mime_list);
+int match_mime(const char *cmd_file_extension, char **mime_list);
 
-void check_mime(char *buf, char *file, int *num_ok, int *num_mismatch) {
+void check_mime(char *mime, char *file, int *num_ok, int *num_mismatch) {
     char *mime_list[NUM_OF_MIMES] = {"pdf", "gif", "jpg", "png", "mp4", "zip", "html"};
-    char *buf_extension;   /* Real extension (from the file command) */
-    char *file_extension;  /* Extension given by the file (not the command) */
+    char *cmd_file_extension;   /* Real extension (from the file command) */
+    char *file_extension;       /* Extension given by the file (not the command) */
     const char slash = '/';
     const char dot = '.';
     int match = 0;
 
    /*
     * Find the last occurrence of the character '/' and '.' in the
-    * strings buf_extension and file_extension respectively.
+    * strings cmd_file_extension and file_extension respectively.
     */
-    if (strrchr(buf, slash) != NULL) buf_extension = strrchr(buf, slash);
+    if (strrchr(mime, slash) != NULL) cmd_file_extension = strrchr(mime, slash);
     if (strrchr(file, dot) != NULL) file_extension = strrchr(file, dot);
 
    /*
-    * Since the strings buf_extension and file_extension start with the
+    * Since the strings cmd_file_extension and file_extension start with the
     * character '/' and '.' respectively, we need to remove that characters
     * in order to compare both strings and the code below.
     */
-    if (buf_extension[0] == '/') buf_extension++;
+    if (cmd_file_extension[0] == '/') cmd_file_extension++;
     if (file_extension[0] == '.') file_extension++;
 
    /*
     * Handle the special case of the mime type JPG, since it comes as JPEG
-    * so it's necessary to change the buf_extension from JPEG to JPG to match
+    * so it's necessary to change the cmd_file_extension from JPEG to JPG to match
     * in the verifications made below in the function match_mime().
     */
-    if (strcmp(file_extension, "jpg") == 0 && strcmp(buf_extension, "jpeg") == 0) memcpy(buf_extension, file_extension, strlen(file_extension)+1);
+    if (strcmp(file_extension, "jpg") == 0 && strcmp(cmd_file_extension, "jpeg") == 0) memcpy(cmd_file_extension, file_extension, strlen(file_extension) + 1);
 
-    match = match_mime(buf_extension, mime_list);
+    match = match_mime(cmd_file_extension, mime_list);
 
     if (match != -1) {
-        /* Comparing the buf_extension with the file_extension to verified if the file is legit */
-        if (strcmp(buf_extension, file_extension) == 0) {
-            printf("[OK] '%s': extension '%s' matches file type '%s'\n", file, file_extension, buf_extension);
+        /* Comparing the cmd_file_extension with the file_extension to verified if the file is legit */
+        if (strcmp(cmd_file_extension, file_extension) == 0) {
+            printf("[OK] '%s': extension '%s' matches file type '%s'\n", file, file_extension, cmd_file_extension);
             *num_ok += 1;
         } else {
-            printf("[MISMATCH] '%s': extension is '%s', file type is '%s'\n", file, file_extension, buf_extension);
+            printf("[MISMATCH] '%s': extension is '%s', file type is '%s'\n", file, file_extension, cmd_file_extension);
             *num_mismatch += 1;
         }
     } else {
-        printf("[INFO] '%s': type '%s' is not supported by checkFile\n", file, buf);
+        printf("[INFO] '%s': type '%s' is not supported by checkFile\n", file, mime);
     }
 }
 
 /*
- * Comparing the buf_extension given by the command file to the
+ * Comparing the cmd_file_extension given by the command file to the
  * array of mime types createad in the begging of the function main.
  */
-int match_mime(const char *buf_extension, char **mime_list) {
+int match_mime(const char *cmd_file_extension, char **mime_list) {
     for (int i = 0; i < NUM_OF_MIMES; i++) {
-        if (strcmp(buf_extension, mime_list[i]) == 0) {
+        if (strcmp(cmd_file_extension, mime_list[i]) == 0) {
             return i;
         }
     }
